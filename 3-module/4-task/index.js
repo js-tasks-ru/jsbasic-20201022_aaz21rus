d639a73fa93176180117d6957fd8938135b8b404
Dmitriy <Aaz21rus@mail.ru>
2020-11-04 19:32:11 +0300

    module5 hw

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -6,16 +6,12 @@
  */
 function showSalary(users, age) {
   const sortUsers = users.filter(item => (item.age <= age))
-    .map(item => {
-      `${item.name} ${item.balance}\n`
-      // for (let i = 0; i < item.length; i++) {
-      //     if(i = item.length) {
-      //         item[i].name + ' ' + item[i].balance
-      //     }
-      //     `${item[i].name} ${item[i].balance}'\n`
-      // }
-    })
-
-  const str = sortUsers.join('')
-  console.log(str)
+  const showUsers = sortUsers.map((item, index) => {
+    if(index === sortUsers.length-1) {
+      return item.name + ', ' + item.balance
+    } else {
+      return `${item.name}, ${item.balance}\n`
+    }
+  }).join('')
+  return showUsers
 }
