a3b61f11afd49f938164d0745eea67dcf13307d4
Dmitriy <Aaz21rus@mail.ru>
2020-11-06 23:14:03 +0300

    module4 hw1,2

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -10,16 +10,16 @@ function highlight(table) {
     } else {
       row.cells[3].classList.add('unavailable');
     }
-    // if (!row.cells[3].hasAttribute('data-available')) {
-    //   row.cells[3].setAttribute('hidden', true)
-    // }
-    // if (row.cells[2].innerHTML === 'm') {
-    //   row.cells[2].classList.add('male')
-    // } else {
-    //   row.cells[2].classList.add('female')
-    // }
-    // if (row.cells[1].innerHTML < 18) {
-    //   row.cells[1].style.textDecoration = 'line-through'
-    // }
+    if (!row.cells[3].hasAttribute('data-available')) {
+      row.cells[3].setAttribute('hidden', true)
+    }
+    if (row.cells[2].innerHTML === 'm') {
+      row.cells[2].classList.add('male')
+    } else {
+      row.cells[2].classList.add('female')
+    }
+    if (row.cells[1].innerHTML < 18) {
+      row.cells[1].style.textDecoration = 'line-through'
+    }
   }
 }
