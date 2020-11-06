e7e742957208c8b6874a451009bee13738b6f368
Dmitriy <Aaz21rus@mail.ru>
2020-11-06 22:58:35 +0300

    module4 hw1,2

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,23 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i]
+    if (row.cells[3].getAttribute('data-available') === 'true') {
+      row.cells[3].classList.add('available')
+    } else {
+      row.cells[3].classList.add('unavailable');
+    }
+    // if (!row.cells[3].hasAttribute('data-available')) {
+    //   row.cells[3].setAttribute('hidden', true)
+    // }
+    // if (row.cells[2].innerHTML === 'm') {
+    //   row.cells[2].classList.add('male')
+    // } else {
+    //   row.cells[2].classList.add('female')
+    // }
+    // if (row.cells[1].innerHTML < 18) {
+    //   row.cells[1].style.textDecoration = 'line-through'
+    // }
+  }
 }
